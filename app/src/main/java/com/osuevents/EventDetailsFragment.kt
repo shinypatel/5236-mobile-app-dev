f0c7528017052d85a6a2ca0afb39911b72120710
Shiny Patel <patelshiny44@gmail.com>
2017-10-15 00:38:02 -0400

    implemented maps and url buttons in event details frag

--- app/src/main/java/com/osuevents/EventDetailsFragment.kt
+++ app/src/main/java/com/osuevents/EventDetailsFragment.kt
@@ -1,10 +1,13 @@
 package com.osuevents
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class EventDetailsFragment : Fragment() {
 
@@ -15,7 +18,24 @@ class EventDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_event_details, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_event_details, container, false)
+
+        val url = view.findViewById<TextView>(R.id.event_url).text.toString()
+        val urlButton = view.findViewById<TextView>(R.id.url_button)
+        urlButton.setOnClickListener{
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
+        }
+
+        val location = view.findViewById<TextView>(R.id.event_location).text.toString()
+        val mapLocation = "http://maps.google.co.in/maps?q=" + location
+        val mapButton = view.findViewById<TextView>(R.id.map_button)
+        mapButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(mapLocation))
+            startActivity(intent)
+        }
+
+        return view
     }
 
 }
