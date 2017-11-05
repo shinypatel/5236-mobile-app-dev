cd62c6fef6d88e1569b2282a030b0ef2942ca10c
Shiny Patel <patelshiny44@gmail.com>
2017-11-05 00:32:53 -0400

    completed map, cal and gps with permissions

--- app/src/main/java/com/osuevents/EventDetailsFragment.kt
+++ app/src/main/java/com/osuevents/EventDetailsFragment.kt
@@ -1,26 +1,25 @@
 package com.osuevents
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
-import android.util.Log
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import java.util.Date
+import android.widget.Toast
 import java.text.SimpleDateFormat
 import java.util.*
-import android.content.Context.LOCATION_SERVICE
-import android.location.LocationManager
-
-
 
 class EventDetailsFragment : Fragment() {
-    val TAG: String = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -28,9 +27,9 @@ class EventDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val intent = activity.intent;
         val view = inflater!!.inflate(R.layout.fragment_event_details, container, false)
 
+        val intent = activity.intent;
         val url = view.findViewById<TextView>(R.id.event_url).text.toString()
         val urlButton = view.findViewById<TextView>(R.id.url_button)
         urlButton.setOnClickListener{
@@ -38,12 +37,23 @@ class EventDetailsFragment : Fragment() {
             startActivity(browserIntent)
         }
 
-        val location = view.findViewById<TextView>(R.id.event_location).text.toString()
-        val mapLocation = "http://maps.google.co.in/maps?q=" + location
+
+        val eventLoc = view.findViewById<TextView>(R.id.event_location).text.toString()
         val mapButton = view.findViewById<TextView>(R.id.map_button)
         mapButton.setOnClickListener{
-            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(mapLocation))
-            startActivity(mapIntent)
+            if (ContextCompat.checkSelfPermission(activity.applicationContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions()
+            }else {
+                val lm = activity.applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val currentLoc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                val lat = currentLoc.latitude
+                val lng = currentLoc.longitude
+                val loc = "http://maps.google.com/maps?saddr:$lat,$lng&daddr=$eventLoc"
+                val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(loc))
+                startActivity(mapIntent)
+            }
         }
 
 
@@ -81,20 +91,52 @@ class EventDetailsFragment : Fragment() {
 
         val calButton = view.findViewById<TextView>(R.id.calender_button)
         calButton.setOnClickListener{
-            val calIntent = Intent(Intent.ACTION_EDIT)
-            calIntent.type = "vnd.android.cursor.item/event"
-            calIntent.putExtra("eventLocation", location)
-            calIntent.putExtra("title", title.text.toString())
-            calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTimeCal.timeInMillis)
-            if(allDay == "true"){
-                calIntent.putExtra("allDay", true)
-            }else{
-                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTimeCal.timeInMillis)
+            if (ContextCompat.checkSelfPermission(activity.applicationContext,
+                    Manifest.permission.READ_CALENDAR)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(activity.applicationContext,
+                            Manifest.permission.WRITE_CALENDAR)
+                            != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions()
+            }else {
+                val calIntent = Intent(Intent.ACTION_EDIT)
+                calIntent.type = "vnd.android.cursor.item/event"
+                calIntent.putExtra("eventLocation", eventLoc)
+                calIntent.putExtra("title", title.text.toString())
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTimeCal.timeInMillis)
+                if (allDay == "true") {
+                    calIntent.putExtra("allDay", true)
+                } else {
+                    calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTimeCal.timeInMillis)
+                }
+                startActivity(calIntent)
             }
-            startActivity(calIntent)
         }
 
         return view
     }
 
+    fun requestPermissions(){
+        var permissions = arrayOf(Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(activity, permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+                }
+                return
+            }
+        }
+    }
+
 }
+
+
